613ff3861236ee5ecbb3d6c220ac37c14449c098
Mike <xxmixanikxx@gmail.com>
2022-12-01 11:08:53 +0200

    use Path.parent instead of /../

--- inputs/__init__.py
+++ inputs/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def get_file_data(file_name, split_lines=True):
-    file_abs_path = f'{Path(__file__)}/../{file_name}.txt'
+    file_abs_path = f'{Path(__file__).parent}/{file_name}.txt'
 
     with open(file_abs_path, 'r', encoding='UTF-8') as file:
         if split_lines:
