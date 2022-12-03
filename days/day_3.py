79f9a0d8aa31f4b0b1c83029e2e8c639c9a7c2b7
Mike <xxmixanikxx@gmail.com>
2022-12-03 23:54:20 +0200

    more day 3 improvements

--- days/day_3.py
+++ days/day_3.py
@@ -7,7 +7,7 @@ def part_1(data: List[str]):
 
     for rucksack in data:
         hs = len(rucksack) // 2
-        common = list(set(rucksack[:hs]) & set(rucksack[hs:]))[0]
+        common ,= set(rucksack[:hs]) & set(rucksack[hs:])
         result += ascii_letters.index(common) + 1
 
     return result
@@ -17,7 +17,7 @@ def part_2(data: List[str]):
     result = 0
 
     for i in range(0, len(data), 3):
-        common = list(set(data[i]) & set(data[i + 1]) & set(data[i + 2]))[0]
+        common ,= set(data[i]) & set(data[i + 1]) & set(data[i + 2])
         result += ascii_letters.index(common) + 1
 
     return result
