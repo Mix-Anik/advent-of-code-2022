c96b23c81e5c4b0226a42437ce1bb195ad89de12
Mike <xxmixanikxx@gmail.com>
2022-12-04 08:17:24 +0200

    day 4 improvements

--- days/day_4.py
+++ days/day_4.py
@@ -10,19 +10,12 @@ def part_2(data: List[str]):
 
 
 def solve(data: List[str]):
-    part1 = 0
-    part2 = 0
+    part1 = part2 = 0
 
     for pairs in data:
-        p1, p2 = pairs.split(',')
-        x1, y1 = [int(s) for s in p1.split('-')]
-        x2, y2 = [int(s) for s in p2.split('-')]
+        x1, y1, x2, y2 = [int(s) for s in pairs.replace(',', '-').split('-')]
         r1, r2 = set(range(x1, y1 + 1)), set(range(x2, y2 + 1))
-
-        if r1.issubset(r2) or r2.issubset(r1):
-            part1 += 1
-
-        if r1.intersection(r2):
-            part2 += 1
+        part1 += int(r1 <= r2 or r2 <= r1)
+        part2 += int(bool(r1 & r2))
 
     return [part1, part2]
