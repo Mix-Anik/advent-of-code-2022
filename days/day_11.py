80aa24bb23b211af15fa62f79776dd5853388b9b
mihhpl <mihhail.plohhotnitsenko@redfunction.ee>
2022-12-11 09:35:43 +0200

    refactored day 11 solution

--- days/day_11.py
+++ days/day_11.py
@@ -3,57 +3,45 @@ from math import floor, lcm
 
 
 def part_1(data: str):
-    items = re.findall(r'Starting items: ((?:\d+,?\s?)*)', data)
-    monkeys = {i: [int(x.strip()) for x in items[i].split(',')] for i in range(len(items))}
-    operations = re.findall(r'Operation: new = (.*)?', data)
-    tests = re.findall(r'divisible by (\d+)?', data)
-    tests_actions = re.findall(r'If true: throw to monkey (\d+)\n\s+If false: throw to monkey (\d+)', data)
-    inspected = {i: 0 for i in range(len(monkeys))}
-
-    for r in range(20):
-        for i in range(len(monkeys)):
-            inspected[i] += len(monkeys[i])
-
-            for old in monkeys[i]:
-                new_wl = eval(operations[i])
-                new_wl = floor(new_wl / 3)
-                throw_to = int(tests_actions[i][int(new_wl % int(tests[i]) != 0)])
-                monkeys[throw_to].append(new_wl)
-
-            monkeys[i] = []
-
-    si = sorted(inspected.values())
-
-    return si[-1] * si[-2]
+    return solve(data, 20, True)
 
 
 def part_2(data: str):
-    items = re.findall(r'Starting items: ((?:\d+,?\s?)*)', data)
-    monkeys = {i: [int(x.strip()) for x in items[i].split(',')] for i in range(len(items))}
+    return solve(data, 10000, False)
+
+
+def solve(data: str, rounds: int, divide_wl: bool):
+    monkeys = {i: {} for i in range(len(data.split('\n\n')))}
+    items = [[int(val.strip()) for val in it.split(',')] for it in re.findall(r'Starting items: ((?:\d+,?\s?)*)', data)]
     operations = re.findall(r'Operation: new = (.*)?', data)
-    tests = re.findall(r'divisible by (\d+)?', data)
-    tests_actions = re.findall(r'If true: throw to monkey (\d+)\n\s+If false: throw to monkey (\d+)', data)
-    inspected = {i: 0 for i in range(len(monkeys))}
-    lcm_value = lcm(*[int(x) for x in tests])
+    divisors = [int(val) for val in re.findall(r'divisible by (\d+)?', data)]
+    actions = [(int(f), int(t)) for t, f in
+               re.findall(r'If true: throw to monkey (\d+)\n\s+If false: throw to monkey (\d+)', data)]
 
-    for r in range(10000):
-        for i in range(len(monkeys)):
-            inspected[i] += len(monkeys[i])
+    for i in range(len(monkeys)):
+        monkeys[i] = {
+            'items': items[i],
+            'ops': operations[i],
+            'divisors': divisors[i],
+            'actions': actions[i],
+            'inspected': 0
+        }
 
-            for old in monkeys[i]:
-                new_wl = eval(operations[i])
-
-                divisor = int(tests[i])
-                if new_wl % divisor == 0:
-                    throw_to = int(tests_actions[i][0])
-                    monkeys[throw_to].append(new_wl % lcm_value)
-                else:
-                    throw_to = int(tests_actions[i][1])
-                    monkeys[throw_to].append(new_wl)
-
-            monkeys[i] = []
-
-    si = sorted(inspected.values())
+    do_rounds(rounds, monkeys, lcm(*divisors), divide_wl)
+    si = sorted([monkeys[i]['inspected'] for i in range(len(monkeys))])
 
     return si[-1] * si[-2]
 
+
+def do_rounds(amount: int, monkeys: dict, lcm_value: int, divide_wl: bool):
+    for _ in range(amount):
+        for i in range(len(monkeys)):
+            monka = monkeys[i]
+
+            for old in monka['items']:
+                new_wl = floor(eval(monka['ops']) / 3) if divide_wl else eval(monka['ops'])
+                throw_to = monka['actions'][int(new_wl % monka['divisors'] == 0)]
+                monkeys[throw_to]['items'].append(new_wl % lcm_value)
+
+            monka['inspected'] += len(monka['items'])
+            monka['items'] = []
